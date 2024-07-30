13c0ff24fc4d9b56868aff5a54b8a6cdb1b7f993
rustedchip <rustedchip@gmail.com>
2024-07-30 05:19:38 +0000

    dev-cors

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'allowed_methods' => ['GET, POST, PUT, OPTIONS'],
     'allowed_origins' => [env('ENDPOINT_A'),env('ENDPOINT_B'),env('ENDPOINT_C'),env('ENDPOINT_D'),],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Origin, Content-Type, X-Auth-Token , Cookie,Access-Control-Request-Headers'],
+    'allowed_headers' => ['Origin, Content-Type, X-Auth-Token , Cookie,Access-Control-Request-Headers','Authorization'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
