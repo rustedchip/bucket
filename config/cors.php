64297dbca849dab1b195816858af95e50d11d872
rustedchip <rustedchip@gmail.com>
2024-08-04 01:14:54 +0000

    delete-file :: cors-delete-method

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'paths' => ['*'],
-    'allowed_methods' => ['GET, POST, PUT, OPTIONS'],
+    'allowed_methods' => ['GET, POST, PUT, OPTIONS,DELETE'],
     'allowed_origins' => [env('ENDPOINT_A'),env('ENDPOINT_B'),env('ENDPOINT_C'),env('ENDPOINT_D'),],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Origin, Content-Type, X-Auth-Token , Cookie,Access-Control-Request-Headers','Authorization'],
