011dd187e0df226a38ac601f1b777e7d5cd643ba
rustedchip <rustedchip@gmail.com>
2024-08-06 01:21:51 +0000

    download :: content-type

--- app/Http/Controllers/BucketController.php
+++ app/Http/Controllers/BucketController.php
@@ -26,97 +26,119 @@ class BucketController extends Controller
         $path = str_replace(' ', '_', $path);
 
         try {
-          
+
             $request->validate([
                 'file' => 'required|file',
             ]);
 
-           
+
             $file = $request->file('file');
-           
-            if(isset($request->filename)){
+
+            if (isset($request->filename)) {
 
                 $filename = $request->filename;
-                $filename  = str_replace("-", "_", $filename );
+                $filename  = str_replace("-", "_", $filename);
 
 
-                $filePath = $path.'/'.$filename.'.'.$file->getClientOriginalExtension();
-
-            }else{
+                $filePath = $path . '/' . $filename . '.' . $file->getClientOriginalExtension();
+            } else {
 
                 $filename = $file->getClientOriginalName();
-                $filename  = str_replace("-", "_", $filename );
+                $filename  = str_replace("-", "_", $filename);
 
-                
-                $filePath = $path.'/' . $filename;
+
+                $filePath = $path . '/' . $filename;
             }
-            
+
 
             $this->googleCloudStorageService->uploadFile($filePath, file_get_contents($file));
 
-            return response()->json(['message' => 'file-uploaded-successfully','success'=> true,], 200);
+            return response()->json(['message' => 'file-uploaded-successfully', 'success' => true,], 200);
         } catch (Exception $e) {
             Log::error('file-upload-failed :: ' . $e->getMessage());
-            return response()->json(['error' => 'file-upload-failed','success'=> false], 500);
+            return response()->json(['error' => 'file-upload-failed', 'success' => false], 500);
         }
     }
 
 
-    public function download($file,$path)
+    public function download($path, $file)
     {
         $path = str_replace("-", "/", $path);
 
         try {
-            $filePath = $path.'/'. $file;
-
-            $fileStream = $this->googleCloudStorageService->downloadFile($filePath);
+            $fileStream = $this->googleCloudStorageService->downloadFile($path . '/' . $file);
 
             if ($fileStream) {
+                $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+                $mimeType = 'application/octet-stream'; 
+                switch ($extension) {
+                    case 'jpg':
+                    case 'jpeg':
+                        $mimeType = 'image/jpeg';
+                        break;
+                    case 'png':
+                        $mimeType = 'image/png';
+                        break;
+                    case 'gif':
+                        $mimeType = 'image/gif';
+                        break;
+                    case 'pdf':
+                        $mimeType = 'application/pdf';
+                        break;
+                    case 'txt':
+                        $mimeType = 'text/plain';
+                        break;
+                    case 'json':
+                        $mimeType = 'application/json';
+                        break;
+                }
+
                 return Response::make($fileStream, 200, [
-                    'Content-Type' => 'application/octet-stream',
-                    'Content-Disposition' => 'attachment; filename="' . $file . '"',
+                    'Content-Type' => $mimeType,
+                    'Content-Disposition' => 'inline; filename="' . $file . '"',
                 ]);
             }
 
-            return response()->json(['message' => 'file-not-found','success'=> false], 404);
+            return response()->json(['message' => 'file-not-found', 'success' => false], 404);
         } catch (Exception $e) {
             Log::error('file-download-failed :: ' . $e->getMessage());
-            return response()->json(['error' => 'file-download-failed','success'=> false], 500);
+            return response()->json(['error' => 'file-download-failed', 'success' => false], 500);
         }
     }
+
     public function files($path)
     {
         $path = str_replace("-", "/", $path);
 
 
         try {
-           
-            $files = $this->googleCloudStorageService->listFiles($path.'/');
+
+            $files = $this->googleCloudStorageService->listFiles($path . '/');
 
             return response()->json($files, 200);
         } catch (Exception $e) {
             Log::error('file-listing-failed: ' . $e->getMessage());
-            return response()->json(['error' => 'file-listing-failed','success'=> false], 500);
+            return response()->json(['error' => 'file-listing-failed', 'success' => false], 500);
         }
     }
 
-    public function delete(Request $request,$path)
+    public function delete(Request $request, $path)
     {
 
         $path = str_replace("-", "/", $path);
 
         try {
-          
+
             $deleted = $this->googleCloudStorageService->deleteFile($path);
 
             if ($deleted) {
-                return response()->json(['message' => 'file-deleted-successfully','success'=> true,], 200);
+                return response()->json(['message' => 'file-deleted-successfully', 'success' => true,], 200);
             } else {
-                return response()->json(['message' => 'file-not-found','success'=> false,], 404);
+                return response()->json(['message' => 'file-not-found', 'success' => false,], 404);
             }
         } catch (Exception $e) {
             Log::error('File deletion failed: ' . $e->getMessage());
-            return response()->json(['error' => 'file-deletion-failed'.$request['path'],'success'=> false], 500);
+            return response()->json(['error' => 'file-deletion-failed' . $request['path'], 'success' => false], 500);
         }
     }
 }
